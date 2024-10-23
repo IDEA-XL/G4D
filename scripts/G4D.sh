72a01afc951814d847a8a374ce7856ce0736170e
Eddy <112454068+EddyLuo1232@users.noreply.github.com>
2024-10-23 23:02:28 +0800

    Delete scripts directory

--- scripts/G4D.sh
+++
@@ -1,8 +0,0 @@
-cd ..
-python main.py --dataset AutoDAN --mode G4D --model_name gpt-4o-mini
-python main.py --dataset CB-Benign --mode G4D --model_name gpt-4o-mini
-python main.py --dataset MT-bench --mode G4D --model_name gpt-4o-mini
-python main.py --dataset Advbench --mode G4D --model_name gpt-4o-mini
-python main.py --dataset MMLU-pro --mode G4D --model_name gpt-4o-mini
-python main.py --dataset AutoDAN --mode G4D --model_name gpt-4o-mini
-python main.py --dataset CB-RedTeam --mode G4D --model_name gpt-4o-mini
